6baaf595bfa694123b553a0cfe90f55d49ee1583
Franco940 <franco.3271940@gmail.com>
2022-06-15 20:29:51 -0300

    Se agregó la anotación Column con sus parámetros correspondientes

--- src/main/java/com/alkemy/ong/entities/User.java
+++ src/main/java/com/alkemy/ong/entities/User.java
@@ -16,10 +16,20 @@ public class User {
     @Id
     @GeneratedValue(generator = "UUID")
     private String id;
+
+    @Column(name = "firstName", nullable = false)
     private String firstName;
+
+    @Column(name = "lastName", nullable = false)
     private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "photo")
     private String photo;
 
     //@OneToMany
