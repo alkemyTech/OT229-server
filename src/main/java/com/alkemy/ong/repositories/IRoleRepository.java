270661a8da4bb2f93a57fabacc79d34a7d9dc101
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-16 15:11:30 -0300

    pacakge models renamed as entities

--- src/main/java/com/alkemy/ong/repositories/IRoleRepository.java
+++ src/main/java/com/alkemy/ong/repositories/IRoleRepository.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.repositories;
 
-import com.alkemy.ong.models.Role;
+import com.alkemy.ong.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
