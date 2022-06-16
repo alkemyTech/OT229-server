0ef019db4ddd1ab34d191beef6c1afa12d149156
Mariano Vergara <marianovf19@outlook.com>
2022-06-16 15:57:27 -0300

    Se agrego la notacion @Repository

--- src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
@@ -2,8 +2,7 @@ package com.alkemy.ong.repositories;
 import com.alkemy.ong.entities.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Repository
 public interface TestimonialRepository<Testimonial,String> {
-
 
 }
