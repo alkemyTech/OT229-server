7c7618f0e5b60e3a9240294bfcca49c90241abc3
Mariano Vergara <marianovf19@outlook.com>
2022-06-16 15:37:12 -0300

    creation of Testimonial Entity and Testimonial Repository

---
+++ src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.repositories;
+import com.alkemy.ong.entities.Testimonial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TestimonialRepository<Testimonial,String> {
+
+
+}
