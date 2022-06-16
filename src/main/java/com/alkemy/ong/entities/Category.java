aa2c083d7d8d1ea4caa9e7a6e90c0fac9f0e24ca
GuadaRebo <86989556+GuadaRebo@users.noreply.github.com>
2022-06-16 15:59:56 -0300

    Creacion modelo y repositorio base de Categories - ticket 18

---
+++ src/main/java/com/alkemy/ong/entities/Category.java
@@ -0,0 +1,44 @@
+package com.alkemy.ong.entities;
+
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Category {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timeStamp;
+    private boolean softDelete;
+}
