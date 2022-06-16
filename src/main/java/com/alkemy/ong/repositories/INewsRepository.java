e2f787950ce3afa593045636064471d754d67ca3
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-16 14:13:43 -0300

    news base repository creation

---
+++ src/main/java/com/alkemy/ong/repositories/INewsRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.repositories;
+
+import com.alkemy.ong.models.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface INewsRepository extends JpaRepository<News,Long> {
+}
