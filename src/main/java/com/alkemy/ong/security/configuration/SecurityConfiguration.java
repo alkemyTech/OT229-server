2cbdfdc23162e4ae57bc8f8b4f063adfb31c0989
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-16 21:54:45 -0300

    Endpoints secured using Spring Security, based on User roles (Ticket OT229-26).

---
+++ src/main/java/com/alkemy/ong/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.alkemy.ong.security.configuration;
+
+import com.alkemy.ong.utility.GlobalConstants;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Handles all global security configurations like endpoint access restriction and defining authentication,
+ * authorization and encryption policies.
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                // Disabling csrf, needed for session cookie authentication, since a token-based authentication protocol
+                // is going to be used instead.
+                .csrf().disable()
+                // Setting the state policy as stateless, disabling the use of sessions for security context.
+                .sessionManagement(
+                        httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // Endpoint restriction
+                .authorizeRequests(
+                        expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
+                                // Allowing requests to the following endpoints to pass through without authentication
+                                .antMatchers(
+                                        GlobalConstants.Endpoints.REGISTER + "/**",
+                                        GlobalConstants.Endpoints.LOGIN + "/**"
+                                ).permitAll()
+                                // Requiring authentication and certain roles for any endpoint not specified above
+                                .anyRequest().hasAnyRole("ADMIN", "USER")
+                );
+
+    }
+}
---
+++ src/main/java/com/alkemy/ong/utility/GlobalConstants.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.utility;
+
+public abstract class GlobalConstants {
+
+    public static abstract class Endpoints {
+
+        public static final String LOGIN = "/auth/login";
+        public static final String REGISTER = "/auth/register";
+
+    }
+
+}
