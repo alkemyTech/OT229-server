af85f87cbc9f18c5420b6b004867b1356a4d8120
Ezequiel Giussani <exe_cien@hotmail.com>
2022-06-17 18:42:27 -0300

    added application properties and repository annotations

--- src/main/java/com/alkemy/ong/repositories/MembersRepository.java
+++ src/main/java/com/alkemy/ong/repositories/MembersRepository.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.entities.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MembersRepository extends JpaRepository<Members, String> {
 
 }
--- src/main/java/com/alkemy/ong/repositories/OrganizationsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/OrganizationsRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.entities.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationsRepository extends JpaRepository<Organization, String> {
 }
