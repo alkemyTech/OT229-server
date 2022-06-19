38319acb4badedae4e32713119429408231a8b4b
Mariano Vergara <marianovf19@outlook.com>
2022-06-18 22:00:36 -0300

    Se importo Repository de libreria stereotype

--- src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.repositories;
 import com.alkemy.ong.entities.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestimonialRepository<Testimonial,String> {
