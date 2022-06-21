9dee7884f2844138cfee3b55391287802066d9ec
Franco940 <franco.3271940@gmail.com>
2022-06-21 16:03:27 -0300

    Se cambio la anotacion del atributo timestamp a @CreationTimeStamp

--- src/main/java/com/alkemy/ong/entities/User.java
+++ src/main/java/com/alkemy/ong/entities/User.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.entities;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -36,7 +37,7 @@ public class User {
     @JoinColumn(name = "role_id")
     private Role roleId;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
     private Date timeStamps;
     private boolean softDelete;
 }
