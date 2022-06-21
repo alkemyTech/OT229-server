0881f415a20264c565b373633eb4c1d31c751b4f
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-21 14:17:08 -0300

    extends JPA Repository added to Testimonial Repository

--- src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repositories/TestimonialRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestimonialRepository<Testimonial,String> {
+public interface TestimonialRepository extends JpaRepository<Testimonial,String> {
 
 }
