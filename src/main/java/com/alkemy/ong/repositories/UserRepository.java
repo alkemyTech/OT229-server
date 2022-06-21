9f28042b8e282abc8daa546c77105a320a31eaba
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-21 20:10:14 -0300

    UserRepository - added methods needed for login

--- src/main/java/com/alkemy/ong/repositories/UserRepository.java
+++ src/main/java/com/alkemy/ong/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.alkemy.ong.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
