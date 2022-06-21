5f44327182039cb812d7b5e9337c89933049b1b1
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-20 23:45:28 -0300

    changes in repository

--- src/main/java/com/alkemy/ong/repositories/INewsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/INewsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface INewsRepository extends JpaRepository<News,Long> {
+public interface INewsRepository extends JpaRepository<News,String> {
 }
