19ffdfdfc6ca2cc67894add07565dc83664d8e47
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-21 18:14:14 -0300

    Login endpoint added to AuthController

--- src/main/java/com/alkemy/ong/security/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/security/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.security.controller;
 
 import com.alkemy.ong.entities.User;
+import com.alkemy.ong.security.payload.LoginRequest;
+import com.alkemy.ong.security.service.AuthenticationService;
 import com.alkemy.ong.services.UserService;
+import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +18,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 
 @RestController
-@RequestMapping("/auth")
 @ResponseStatus
 
 public class AuthController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
-    @PostMapping("/register")
+    @PostMapping(GlobalConstants.Endpoints.REGISTER)
     public ResponseEntity<?> register(@Valid @RequestBody SignupRequest signupRequest) {
       try {
           User user = new User();
@@ -41,6 +46,26 @@ public class AuthController {
       } catch (Exception e) {
           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
       }
+    }
+
+    @PostMapping(GlobalConstants.Endpoints.LOGIN)
+    public ResponseEntity<?> login(@Valid LoginRequest loginForm) {
+
+        System.out.println(loginForm.getUsername() + "  " + loginForm.getPassword());
+        try {
+            return ResponseEntity.ok(
+                    this.authenticationService
+                            .authenticate(loginForm.getUsername(), loginForm.getPassword())
+                            .getAuthenticatedUser()
+                            .get()
+            );
+        } catch (AuthenticationException e) { // thrown by authenticationService.authenticate()
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body( new HashMap<String, String>().put("ok", "false") );
+        } catch (IllegalStateException e) { // thrown by authenticationService.getAuthenticatedUser()
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
     
     
