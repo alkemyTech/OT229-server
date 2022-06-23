625693961deae1267cd9045cbffd75ae36b47e44
Franco940 <franco.3271940@gmail.com>
2022-06-23 01:15:42 -0300

    Se creo el DTO de usuario para devolver en el login

---
+++ src/main/java/com/alkemy/ong/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String tokenJWT;
+}
