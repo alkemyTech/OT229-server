7422a1032ac3e0dcffdef2add6124d81ae3d953c
Franco940 <franco.3271940@gmail.com>
2022-06-23 01:20:50 -0300

    Se creo la interfaz para el JWTService

---
+++ src/main/java/com/alkemy/ong/security/service/JwtService.java
@@ -0,0 +1,51 @@
+package com.alkemy.ong.security.service;
+
+import com.alkemy.ong.entities.User;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.List;
+
+public interface JwtService {
+
+    /**
+     * This method return a JWT token with user's info.
+     * - User ID
+     * - User first name
+     * - User last name
+     * - Date creation
+     * - Date expiration
+     * - User roles
+     *
+     * @Param user first name.
+     * @Param user last name.
+     * @Param user roles.
+     * @return a JWT token with user's info.
+     * */
+    String createToken(User user);
+
+    /**
+     * This method check the HEADER of the token.
+     *
+     * @return true if HEADER start with "Bearer "
+     * */
+    Boolean isBearer(String token);
+
+    /**
+     * This method search the username claim in the JWT token
+     * */
+    String getUsername(String token) throws Exception;
+
+    /**
+     * This method search all roles in the JWT token
+     *
+     * @return user roles
+     * */
+    List<String> getRoles(String token) throws Exception;
+
+    /**
+     * This method decrypt the token, check the expiration date and the issuer
+     *
+     * @return JWT token decrypted
+     * */
+    DecodedJWT verify(String token) throws Exception;
+}
