1fe3cec725d8557b898750d8925298758878377b
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-23 00:46:51 -0300

    Credentials configuration implemented

---
+++ src/main/java/com/alkemy/ong/configuration/AmazonS3CredentialsConfiguration.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "aws.s3.properties")
+@Configuration
+public class AmazonS3CredentialsConfiguration {
+
+    private String endpointUrl;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+
+}
