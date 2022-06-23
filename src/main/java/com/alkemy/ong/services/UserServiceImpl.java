ead78bb3a0b6314d6f3754b2b62092dbbceda450
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-23 20:02:40 -0300

    creacion de metodo de borrar usuario en el servicio

--- src/main/java/com/alkemy/ong/services/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.services;
 
 import com.alkemy.ong.entities.User;
 import com.alkemy.ong.repositories.UserRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,19 @@ public class UserServiceImpl implements UserService{
     @Override
     public Optional<User> getUserByEmail(String email) {
         return this.userRepo.findByEmail(email);
+    }
+
+    @Override
+    public String delete(String id) throws NotFoundException {
+        boolean userExistis = userRepo.existsById(id);
+        if(!userExistis)throw new NotFoundException("A user with id "+id+" was not found");
+
+        User u= userRepo.getById(id);
+
+        if(u.isSoftDelete())throw new NotFoundException("Please enter the id of a user without deleting");
+        u.setSoftDelete(true);
+
+        return "Successfully deleted user with id "+id;
     }
 
 
