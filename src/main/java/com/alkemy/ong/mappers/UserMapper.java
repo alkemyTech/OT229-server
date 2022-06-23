d449f6d3f91666f7b3247eea7cbbeedac66be7e6
Franco940 <franco.3271940@gmail.com>
2022-06-23 16:49:17 -0300

    User mapper

---
+++ src/main/java/com/alkemy/ong/mappers/UserMapper.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.mappers;
+
+import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserDTO userEntity2DTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+
+        return userDTO;
+    }
+}
