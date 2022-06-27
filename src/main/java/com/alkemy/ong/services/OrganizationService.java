a3acd069c41db035391367b5e732dbe0a8fb1f7e
Franco940 <franco.3271940@gmail.com>
2022-06-27 18:46:18 -0300

    Se creo el metodo para actualizar una organizacion en la interfaz

--- src/main/java/com/alkemy/ong/services/OrganizationService.java
+++ src/main/java/com/alkemy/ong/services/OrganizationService.java
@@ -1,12 +1,15 @@
 package com.alkemy.ong.services;
 
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.dto.ReducedOrganizationDTO;
 
 import java.util.List;
 
 public interface OrganizationService {
 
-    public List<OrganizationDTO> getAll();
+    public List<ReducedOrganizationDTO> getAll();
 
-    public OrganizationDTO getById (String id);
+    public ReducedOrganizationDTO getById (String id);
+
+    public OrganizationDTO updateOrganization(OrganizationDTO organizationDTO, String organizationName);
 }
