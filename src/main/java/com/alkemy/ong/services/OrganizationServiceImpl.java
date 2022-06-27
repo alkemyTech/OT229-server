7ed9a3c2d678ec1b2a4f9bd346da3911a55e0dea
Franco940 <franco.3271940@gmail.com>
2022-06-27 18:46:40 -0300

    Se le dio logica al metodo de la interfaz para actualizar la información de una organizacion

--- src/main/java/com/alkemy/ong/services/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/OrganizationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.services;
 
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.dto.ReducedOrganizationDTO;
 import com.alkemy.ong.entities.Organization;
 import com.alkemy.ong.mappers.OrganizationMapper;
 import com.alkemy.ong.repositories.OrganizationsRepository;
@@ -19,21 +20,59 @@ public class OrganizationServiceImpl implements OrganizationService{
     @Autowired
     private OrganizationsRepository organizationsRepository;
 
-
-    public List<OrganizationDTO> getAll() {
+    @Override
+    public List<ReducedOrganizationDTO> getAll() {
         List<Organization> entities = organizationsRepository.findAll();
-        List<OrganizationDTO> dtos = organizationMapper.organizationEntity2DTOList(entities);
+        List<ReducedOrganizationDTO> dtos = organizationMapper.organizationEntity2ReducedDTOList(entities);
 
         return dtos;
     }
 
-
-    public OrganizationDTO getById(String id) {
+    @Override
+    public ReducedOrganizationDTO getById(String id) {
         Optional<Organization> entity = organizationsRepository.findById(id);
         if (!entity.isPresent()) {
             throw new RuntimeException("Organization with the provided ID not present");
         }
-        OrganizationDTO dto = organizationMapper.organizationEntity2DTO(entity.get());
+        ReducedOrganizationDTO dto = organizationMapper.organizationEntity2ReducedDTO(entity.get());
         return dto;
+    }
+
+    @Override
+    public OrganizationDTO updateOrganization(OrganizationDTO organizationDTO, String organizationName) {
+        Organization organization = organizationsRepository.findByName(organizationName).get();
+
+        updateInfo(organization, organizationDTO);
+
+        organizationsRepository.save(organization);
+
+        return organizationMapper.organizationDTO2Entity(organization);
+    }
+
+    private Organization updateInfo(Organization organization, OrganizationDTO organizationDTO){
+        // Si el atributo del objeto OrganizationDTO es null, es porque no se quiere actualizar
+        if(organizationDTO.getName() != null && !organization.getName().equalsIgnoreCase(organizationDTO.getName())){
+            organization.setName(organizationDTO.getName());
+        }
+        if(organizationDTO.getImage() != null && !organization.getImage().equalsIgnoreCase(organizationDTO.getImage())){
+            organization.setImage(organizationDTO.getImage());
+        }
+        if(organizationDTO.getPhone() != null && organization.getPhone() != organizationDTO.getPhone()){
+            organization.setPhone(organizationDTO.getPhone());
+        }
+        if(organizationDTO.getAddress() != null && !organization.getAddress().equalsIgnoreCase(organizationDTO.getAddress())){
+            organization.setAddress(organizationDTO.getAddress());
+        }
+        if(organizationDTO.getEmail() != null && !organization.getEmail().equalsIgnoreCase(organizationDTO.getEmail())){
+            organization.setEmail(organizationDTO.getEmail());
+        }
+        if(organizationDTO.getWelcomeText() != null && !organization.getWelcomeText().equalsIgnoreCase(organizationDTO.getWelcomeText())){
+            organization.setWelcomeText(organizationDTO.getWelcomeText());
+        }
+        if(organizationDTO.getAboutUsText() != null && !organization.getAboutUsText().equalsIgnoreCase(organizationDTO.getAboutUsText())){
+            organization.setAboutUsText(organizationDTO.getAboutUsText());
+        }
+
+        return organization;
     }
 }
