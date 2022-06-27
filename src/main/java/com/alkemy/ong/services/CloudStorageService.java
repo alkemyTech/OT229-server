dcadd58836fc39620435762fd3918887a369bb27
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-27 13:53:23 -0300

    Necessary methods added to CloudStorageService interface

--- src/main/java/com/alkemy/ong/services/CloudStorageService.java
+++ src/main/java/com/alkemy/ong/services/CloudStorageService.java
@@ -1,7 +1,40 @@
 package com.alkemy.ong.services;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Interface for the services used to perform file CRUD operations against external cloud storage services.
  */
 public interface CloudStorageService {
+
+    /**
+     * Uploads a file to the external Cloud Storage
+     *
+     * @param multipartFile the file to be uploaded
+     * @return  the absolute url to access the uploaded file
+     * @throws IOException
+     */
+    String uploadFile(MultipartFile multipartFile) throws IOException;
+
+    /**
+     * Deletes a file form the external Cloud Storage
+     *
+     * @param fileUrl   the absolute url of the file to be deleted
+     * @throws IOException
+     */
+    void deleteFileFromS3Bucket(String fileUrl) throws IOException;
+
+    /**
+     * Downloads a file from the cloud storage.
+     *
+     * @param fileUrl   the absolute url of the file.
+     * @return  the stream to the file.
+     * @throws IOException
+     */
+    InputStream downloadFile(String fileUrl) throws IOException;
+
 }
