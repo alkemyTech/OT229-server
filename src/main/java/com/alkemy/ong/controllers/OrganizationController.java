2d33e0cbdcd5897fe03a3be11a01a7f5446868fe
Franco940 <franco.3271940@gmail.com>
2022-06-27 14:55:27 -0300

    Actualizando contentido de la rama con lo nuevo de main

--- src/main/java/com/alkemy/ong/controllers/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controllers/OrganizationController.java
@@ -2,17 +2,17 @@ package com.alkemy.ong.controllers;
 
 import com.alkemy.ong.dto.OrganizationDTO;
 import com.alkemy.ong.services.OrganizationService;
+import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@PreAuthorize("authenticated")
 @RestController
-@RequestMapping("/organization/public")
+@RequestMapping(GlobalConstants.Endpoints.ORGANIZATION_PUBLIC_INFO)
 public class OrganizationController {
 
     @Autowired
@@ -28,5 +28,10 @@ public class OrganizationController {
     public ResponseEntity<OrganizationDTO> getById(@PathVariable String id) {
         OrganizationDTO dto = organizationService.getById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public String actualizarOrganizacion(){
+        return "hola";
     }
 }
