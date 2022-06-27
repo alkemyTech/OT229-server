5cf29877ad3bd4040aaba454bd6efc202ca3315a
Franco940 <franco.3271940@gmail.com>
2022-06-27 18:36:57 -0300

    Se creo el endpoint para cambiar información de una organización

--- src/main/java/com/alkemy/ong/controllers/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controllers/OrganizationController.java
@@ -1,16 +1,19 @@
 package com.alkemy.ong.controllers;
 
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.dto.ReducedOrganizationDTO;
 import com.alkemy.ong.services.OrganizationService;
 import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@PreAuthorize("authenticated")
+//@PreAuthorize("permitAll()") // Descomentar cuando se trabaje en el ticket 73
 @RestController
 @RequestMapping(GlobalConstants.Endpoints.ORGANIZATION_PUBLIC_INFO)
 public class OrganizationController {
@@ -19,19 +22,22 @@ public class OrganizationController {
     private OrganizationService organizationService;
 
     @GetMapping
-    public ResponseEntity<List<OrganizationDTO>> getAll() {
-        List<OrganizationDTO> dtos = organizationService.getAll();
+    public ResponseEntity<List<ReducedOrganizationDTO>> getAll() {
+        List<ReducedOrganizationDTO> dtos = organizationService.getAll();
         return ResponseEntity.ok().body(dtos);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrganizationDTO> getById(@PathVariable String id) {
-        OrganizationDTO dto = organizationService.getById(id);
+    public ResponseEntity<ReducedOrganizationDTO> getById(@PathVariable String id) {
+        ReducedOrganizationDTO dto = organizationService.getById(id);
         return ResponseEntity.ok().body(dto);
     }
 
-    @PostMapping
-    public String actualizarOrganizacion(){
-        return "hola";
+    //@PreAuthorize("hasRole('ADMIN')") // Descomentar cuando se trabaje en el ticket 73
+    @PostMapping("/{organizationName}")
+    public ResponseEntity<OrganizationDTO> updateOrganization(@PathVariable String organizationName, @RequestBody @Valid OrganizationDTO organizationDTO){
+        OrganizationDTO org = organizationService.updateOrganization(organizationDTO, organizationName);
+
+        return ResponseEntity.status(HttpStatus.OK).body(org);
     }
 }
