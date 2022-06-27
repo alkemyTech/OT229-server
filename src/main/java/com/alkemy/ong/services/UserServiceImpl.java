ba1baf03db477495060af77890c0de48a9bfc556
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-27 16:04:27 -0300

    the get all method is implemented in the user implementation service

--- src/main/java/com/alkemy/ong/services/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/UserServiceImpl.java
@@ -1,12 +1,17 @@
 
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dto.UserDTO;
 import com.alkemy.ong.entities.User;
+import com.alkemy.ong.mappers.UserMapper;
 import com.alkemy.ong.repositories.UserRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +19,9 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     UserRepository userRepo;
+
+    @Autowired
+    UserMapper mapper;
 
     @Override
     public User save(User user) {
@@ -36,4 +44,15 @@ public class UserServiceImpl implements UserService{
         return "Successfully deleted user with id " + id;
     }
 
+    @Override
+    public List<UserDTO> getAll() {
+
+        List<UserDTO> usersDTO = new LinkedList<>();
+        for(User user : userRepo.findAll()){
+            usersDTO.add(mapper.userEntity2DTO(user));
+        }
+        usersDTO.sort(Comparator.comparing(UserDTO::getEmail).reversed());
+        return usersDTO;
+
+    }
 }
