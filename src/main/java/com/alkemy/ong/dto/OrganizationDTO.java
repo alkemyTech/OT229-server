2a3e30ad6d92b6e5b7d1800fe972348c64682edc
Franco940 <franco.3271940@gmail.com>
2022-06-27 18:37:29 -0300

    Se creo un DTO para la entidad Organization con más atributos

--- src/main/java/com/alkemy/ong/dto/OrganizationDTO.java
+++ src/main/java/com/alkemy/ong/dto/OrganizationDTO.java
@@ -1,17 +1,42 @@
 package com.alkemy.ong.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+@NoArgsConstructor
 @Getter
 @Setter
 public class OrganizationDTO {
 
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "The organization name has to be a String")
+    @Nullable
     private String name;
 
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "The organization image has to be a String")
+    @Nullable
     private String image;
 
-    private int phone;
+    @Nullable
+    private Integer phone;
 
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "The organization address has to be a String")
+    @Nullable
     private String address;
+
+    @Email
+    @Nullable
+    private String email;
+
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "The organization welcomeText has to be a String")
+    @Nullable
+    private String welcomeText;
+
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "The organization aboutUsText has to be a String")
+    @Nullable
+    private String aboutUsText;
 }
