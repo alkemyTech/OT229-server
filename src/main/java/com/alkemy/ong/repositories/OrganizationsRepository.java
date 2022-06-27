55ef8ffd61c084eb0f03bb70486ff3f02bd0d48a
Franco940 <franco.3271940@gmail.com>
2022-06-27 18:40:07 -0300

    Metodo para buscar una organizacion por nombre

--- src/main/java/com/alkemy/ong/repositories/OrganizationsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/OrganizationsRepository.java
@@ -4,6 +4,9 @@ import com.alkemy.ong.entities.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrganizationsRepository extends JpaRepository<Organization, String> {
+    Optional<Organization> findByName(String name);
 }
