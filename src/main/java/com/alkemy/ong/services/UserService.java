63d9c828c64bab6ce59452dbe8440b5af593060e
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-27 22:46:34 -0300

    added update user method to user service interface

--- src/main/java/com/alkemy/ong/services/UserService.java
+++ src/main/java/com/alkemy/ong/services/UserService.java
@@ -1,9 +1,13 @@
 
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.dto.UserDTORequest;
 import com.alkemy.ong.entities.User;
 import javassist.NotFoundException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 
@@ -13,5 +17,5 @@ public interface UserService {
   public User save(User user);
   Optional<User> getUserByEmail(String email);
   String delete(String id) throws NotFoundException;
-
+  UserDTO updateUser(MultipartFile file, UserDTORequest userDTOrequest) throws NotFoundException, IOException;
 }
