2343169cf2a617144f4f353dc78150cbf6a1f0ad
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-27 22:43:57 -0300

    userDTORequest class was created

---
+++ src/main/java/com/alkemy/ong/dto/UserDTORequest.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDTORequest {
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Email(message = "Invalid email format")
+    private String email;
+    @Size(min = 1, max = 255)
+    private String password;
+
+
+}
