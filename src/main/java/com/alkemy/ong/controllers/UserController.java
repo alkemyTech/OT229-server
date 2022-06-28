deec2327032b760dc142d48eec292550a6376bba
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-27 22:51:58 -0300

    created endpoint to update user in the controller

--- src/main/java/com/alkemy/ong/controllers/UserController.java
+++ src/main/java/com/alkemy/ong/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.alkemy.ong.controllers;
 
+import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.dto.UserDTORequest;
 import com.alkemy.ong.services.UserService;
 import com.alkemy.ong.utility.GlobalConstants;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(GlobalConstants.Endpoints.USER)
@@ -27,5 +32,15 @@ public class UserController {
         }
     }
 
+    @PutMapping
+    public ResponseEntity<UserDTO> updateUser(@RequestParam(value = "file", required = false) MultipartFile multipartfile, @ModelAttribute UserDTORequest userDTORequest) {
 
+        try {
+            return new ResponseEntity<>(userService.updateUser(multipartfile, userDTORequest), HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
