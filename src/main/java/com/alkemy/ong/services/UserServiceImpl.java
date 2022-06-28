6a32a9f01f58c4beef06f7cb7c77befb153f42ca
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-27 22:49:52 -0300

    The update user method was implemented in the service implementation, with the image upload

--- src/main/java/com/alkemy/ong/services/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/UserServiceImpl.java
@@ -1,12 +1,18 @@
 
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.dto.UserDTORequest;
 import com.alkemy.ong.entities.User;
+import com.alkemy.ong.mappers.UserMapper;
 import com.alkemy.ong.repositories.UserRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -14,6 +20,15 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     UserRepository userRepo;
+
+    @Autowired
+    UserMapper mapper;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AmazonS3Service amazonS3Service;
 
     @Override
     public User save(User user) {
@@ -34,6 +49,23 @@ public class UserServiceImpl implements UserService{
         userRepo.deleteById(id);
 
         return "Successfully deleted user with id " + id;
+    }
+
+    @Override
+    public UserDTO updateUser(MultipartFile file, UserDTORequest userDTOrequest) throws NotFoundException, IOException {
+        Boolean exists= userRepo.existsById(userDTOrequest.getId());
+        if(!exists) throw new NotFoundException("A user with id " + userDTOrequest.getId() + " was not found");
+        User user = userRepo.getById(userDTOrequest.getId());
+
+        if(!userDTOrequest.getEmail().isEmpty()) user.setEmail(userDTOrequest.getEmail());
+        if(!userDTOrequest.getFirstName().isEmpty()) user.setFirstName(userDTOrequest.getFirstName());
+        if(!userDTOrequest.getLastName().isEmpty()) user.setLastName(userDTOrequest.getLastName());
+        if(!userDTOrequest.getPassword().isEmpty()) user.setPassword(passwordEncoder.encode(userDTOrequest.getPassword()));
+        if(!file.isEmpty()) user.setPhoto(amazonS3Service.uploadFile(file));
+
+        userRepo.save(user);
+
+        return mapper.userEntity2DTO(user);
     }
 
 }
