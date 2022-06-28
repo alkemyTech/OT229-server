6a93e863f0614d307225cba6002842ec867cf5b8
Franco940 <franco.3271940@gmail.com>
2022-06-28 20:24:43 -0300

    Se puso la logica de crear usuario en el servicio correspondiente

--- src/main/java/com/alkemy/ong/security/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/security/controller/AuthController.java
@@ -1,30 +1,24 @@
 package com.alkemy.ong.security.controller;
 
-import com.alkemy.ong.entities.User;
+import com.alkemy.ong.dto.UserDTO;
 import com.alkemy.ong.security.payload.LoginRequest;
 import com.alkemy.ong.security.service.AuthenticationService;
-import com.alkemy.ong.services.EmailService;
 import com.alkemy.ong.services.UserService;
+import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.alkemy.ong.security.payload.SignupRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.util.HashMap;
 
 
 @RestController
-@RequestMapping("/auth")
 @ResponseStatus
-
 public class AuthController {
 
     @Autowired
@@ -33,26 +27,21 @@ public class AuthController {
     private AuthenticationService authenticationService;
     @Autowired
     private UserService userService;
-    @Autowired
-    private EmailService emailService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody SignupRequest signupRequest) {
+    @PostMapping(GlobalConstants.Endpoints.REGISTER)
+    public ResponseEntity<?> register(@RequestParam(value="file", required = false) MultipartFile image,
+                                      @ModelAttribute SignupRequest signupRequest) {
       try {
-          User user = new User();
-          user.setFirstName(signupRequest.getFirstname());
-          user.setLastName(signupRequest.getLastname());
-          user.setEmail(signupRequest.getEmail());
-          user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
-          userService.save(user);
-          emailService.sendEmail(signupRequest.getEmail());
-          return new ResponseEntity(user, HttpStatus.CREATED);
+          UserDTO userDTO = userService.createUser(signupRequest, image);
+
+
+          return new ResponseEntity(userDTO, HttpStatus.CREATED);
       } catch (Exception e) {
           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
       }
     }
 
-    @PostMapping("/login")
+    @PostMapping(GlobalConstants.Endpoints.LOGIN)
     public ResponseEntity<?> login(@Valid LoginRequest loginForm) {
 
         System.out.println(loginForm.getUsername() + "  " + loginForm.getPassword());
