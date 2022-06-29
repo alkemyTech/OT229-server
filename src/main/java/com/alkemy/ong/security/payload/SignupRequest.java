b03737bbe6bb1a45de0d364e776d894d6bae19ed
Franco940 <franco.3271940@gmail.com>
2022-06-28 23:37:55 -0300

    Se pusieron agregaron las validaciones en el SingupRequest

--- src/main/java/com/alkemy/ong/security/payload/SignupRequest.java
+++ src/main/java/com/alkemy/ong/security/payload/SignupRequest.java
@@ -3,14 +3,21 @@ package com.alkemy.ong.security.payload;
 import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class SignupRequest {
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
 
+    @NotBlank
     @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank
     private String password;
 }
