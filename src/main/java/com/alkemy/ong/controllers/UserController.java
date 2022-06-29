b9a1c609fa74bb6237f1b51b3208eb4459cd211c
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-29 16:05:13 -0300

    Changes user controller and service (#47)
    * changes in user service and controller, email unique
    
    * changes in delete htttp status

--- src/main/java/com/alkemy/ong/controllers/UserController.java
+++ src/main/java/com/alkemy/ong/controllers/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
     @DeleteMapping
     public String deleteUser(@RequestParam("id") String id, HttpServletResponse httpServletResponse) {
         try {
-            httpServletResponse.setStatus(HttpStatus.OK.value());
+            httpServletResponse.setStatus(HttpStatus.NO_CONTENT.value());
             return userService.delete(id);
         } catch (NotFoundException e) {
             httpServletResponse.setStatus(HttpStatus.NOT_FOUND.value());
@@ -45,15 +45,17 @@ public class UserController {
     }
 
  @PutMapping
-    public ResponseEntity<UserDTO> updateUser(@RequestParam(value = "file", required = false) MultipartFile multipartfile, @ModelAttribute UserDTORequest userDTORequest) {
+    public ResponseEntity<?> updateUser(@RequestParam(value = "file", required = false) MultipartFile multipartfile, @ModelAttribute UserDTORequest userDTORequest) {
         try {
             return new ResponseEntity<>(userService.updateUser(multipartfile, userDTORequest), HttpStatus.OK);
         } catch (NotFoundException e) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
          } catch (AmazonS3Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).build();
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(e.getMessage());
         } catch (IOException e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
     @GetMapping("/auth/me")
