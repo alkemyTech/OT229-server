d5b08045da78d215de2f3e73dca5c654ccf4494d
Franco940 <franco.3271940@gmail.com>
2022-06-28 23:31:37 -0300

    Se agregó la lógica para crear el rol ROLE_USER si no existe

--- src/main/java/com/alkemy/ong/services/impl/RoleServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/RoleServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -16,11 +17,30 @@ public class RoleServiceImpl implements RoleService {
     RoleRepository roleRepository;
 
     @Override
-    public Set<Role> findRoleUser() {
+    public Set<Role> getRoleUser() {
         // Rol: ROLE_USER
+        Optional<Role> roleFound = roleRepository.findByName(GlobalConstants.ROLE_USER);
         Set<Role> roles = new LinkedHashSet<>();
-        roles.add(roleRepository.findByName(GlobalConstants.ROLE_USER).get());
+
+        if(roleFound.isPresent()){
+            roles.add(roleFound.get());
+        }else{
+            Role role = creationRoleUser();
+            roles.add(role);
+        }
 
         return roles;
+    }
+
+    @Override
+    public Role creationRoleUser(){
+        Role role = new Role();
+
+        role.setName(GlobalConstants.ROLE_USER);
+        role.setDescription("This rol is for the common user.");
+
+        roleRepository.save(role);
+
+        return role;
     }
 }
