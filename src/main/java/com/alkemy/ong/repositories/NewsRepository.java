010b843990b6a33e5952850220bb8e21cef737bb
Ivan Pizarro <pizarroivan89@gmail.com>
2022-06-30 20:17:34 -0300

    Explicit find method declared in NewsRepository

--- src/main/java/com/alkemy/ong/repositories/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/NewsRepository.java
@@ -4,6 +4,11 @@ import com.alkemy.ong.entities.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NewsRepository extends JpaRepository<News,String> {
+
+    Optional<News> findById(String id);
+
 }
