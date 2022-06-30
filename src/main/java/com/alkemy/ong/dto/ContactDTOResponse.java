b79eb2947828c29c709b84fff355061f4f64fe3d
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-06-30 14:01:40 -0300

    Ot229 53 Crear modelo, repositorio y servicio de contacto. (#52)
    * Contact entity creation
    
    * Contact Repository creation
    
    * service, mapper,dtos.Contact
    
    * added @Service in ContactServiceImpl

---
+++ src/main/java/com/alkemy/ong/dto/ContactDTOResponse.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContactDTOResponse {
+
+    private String name;
+    private String email;
+    private String confirmation;
+}
