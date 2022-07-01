fa4d6bb62309d8106cb43ac8a80467f61e6cc7bd
Franco Roman <franco.3271940@gmail.com>
2022-06-30 23:35:18 -0300

    Se creó la lógica para el método de buscar slide por id

--- src/main/java/com/alkemy/ong/services/impl/SlidesServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/SlidesServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.services.impl;
 
 import com.alkemy.ong.dto.SlidesEntityDTO;
+import com.alkemy.ong.entities.SlidesEntity;
 import com.alkemy.ong.mappers.SlidesEntityMapper;
 import com.alkemy.ong.repositories.SlideRepository;
 import com.alkemy.ong.services.SlidesService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,6 +26,17 @@ public class SlidesServiceImpl implements SlidesService {
                 .stream()
                 .map(this.slidesMapper::entityToDto)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public SlidesEntityDTO findById(String slideId) throws RuntimeException {
+        Optional<SlidesEntity> slideFound = slideRepository.findById(slideId);
+
+        if(slideFound.isPresent()){
+            return slidesMapper.entityToDto(slideFound.get());
+        }else{
+            throw new RuntimeException("Slide with the provided ID not present");
+        }
     }
 
 }
