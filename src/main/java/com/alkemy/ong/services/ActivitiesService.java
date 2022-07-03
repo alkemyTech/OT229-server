1203753758f447fcf6e02373e706b8c027ead240
exerazor1911 <ezequielgiussani56@gmail.com>
2022-07-03 09:58:38 -0300

    Refactores Activity Service

--- src/main/java/com/alkemy/ong/services/ActivitiesService.java
+++ src/main/java/com/alkemy/ong/services/ActivitiesService.java
@@ -1,14 +1,16 @@
 package com.alkemy.ong.services;
 
 import com.alkemy.ong.dto.ActivityDTO;
-import org.springframework.http.ResponseEntity;
+import com.alkemy.ong.exception.ActivityNamePresentException;
+import com.alkemy.ong.exception.ActivityNotFoundException;
+import com.alkemy.ong.exception.AmazonS3Exception;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface ActivitiesService {
 
-    public ActivityDTO save (MultipartFile file, ActivityDTO dto) throws IOException;
+    public ActivityDTO save (MultipartFile file, ActivityDTO dto) throws IOException, AmazonS3Exception, ActivityNamePresentException;
 
-    public ActivityDTO edit (MultipartFile file, ActivityDTO dto, String id) throws IOException;
+    public ActivityDTO edit (MultipartFile file, ActivityDTO dto, String id) throws IOException, AmazonS3Exception, ActivityNamePresentException, ActivityNotFoundException;
 }
