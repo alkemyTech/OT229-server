512ca289b9d3ced9d793f368a3bcf88e2092deb6
Camilo Abarca <cabarcaz@yahoo.cl>
2022-07-05 13:35:59 -0400

    se crea metodo delete

--- src/main/java/com/alkemy/ong/controllers/SlidesController.java
+++ src/main/java/com/alkemy/ong/controllers/SlidesController.java
@@ -1,16 +1,19 @@
 package com.alkemy.ong.controllers;
 
+import com.alkemy.ong.dto.DeleteEntityResponse;
 import com.alkemy.ong.dto.ReducedSlideDTO;
 import com.alkemy.ong.dto.SlidesEntityDTO;
 import com.alkemy.ong.services.CloudStorageService;
 import com.alkemy.ong.services.SlidesService;
 import com.alkemy.ong.utility.GlobalConstants;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -50,6 +53,17 @@ public class SlidesController {
         }catch (RuntimeException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<?>deleteSlide(@PathVariable String id) throws NotFoundException {
+        try {
+            SlidesEntityDTO slideDTO = this.slidesService.deleteSlide(id);
+            return ResponseEntity.ok(new DeleteEntityResponse("Slide successful deleted",slideDTO));
+        }catch (EntityNotFoundException e ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 
 }
