00b94440cec35e2421bddb62faf44ee88a6fb878
Brenda Daffunchio <brendaffunchio@gmail.com>
2022-07-07 20:55:10 -0300

    Ot229 66 Actualizacion de testimonios (#80)
    * OT229-66: Added method to update testimonials in service
    
    * OT229-66: Added endpoint to update testimonials in controller
    
    * fixes
    
    * added required=false in request param. Endpoint update testimonials

--- src/main/java/com/alkemy/ong/controllers/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controllers/TestimonialController.java
@@ -5,6 +5,7 @@ import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.exception.CorruptedFileException;
 import com.alkemy.ong.services.TestimonialService;
 import com.alkemy.ong.utility.GlobalConstants;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +26,15 @@ public class TestimonialController {
                                                @RequestParam("file") MultipartFile file) throws CloudStorageClientException, CorruptedFileException {
         return new ResponseEntity<>(service.create(file,request), HttpStatus.CREATED);
     }
+    @PutMapping
+    public ResponseEntity<?> updateTestimonial(@Valid @ModelAttribute TestimonialDTORequest request,
+                                               @RequestParam(value = "file", required = false) MultipartFile file,
+                                               @RequestParam("id")String id) throws CloudStorageClientException, CorruptedFileException {
+       try{
+           return new ResponseEntity<>(service.update(id,file,request), HttpStatus.OK);
+       } catch (NotFoundException e) {
+         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+       }
 
+    }
 }
