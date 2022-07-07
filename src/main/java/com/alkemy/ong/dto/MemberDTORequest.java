14fae048933027f7d9941da679cac978c40669cd
Mariano Vergara <marianovf19@outlook.com>
2022-07-06 23:54:02 -0300

    Se creo la clase request y responde de Members

---
+++ src/main/java/com/alkemy/ong/dto/MemberDTORequest.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTORequest {
+
+    @NotEmpty(message = "Name not provided")
+    private String name;
+
+    private String facebookUrl;
+
+    private String instagramUrl;
+
+    private String linkedinUrl;
+
+    private String image;
+
+    private String description;
+}
