3d59bcad147c47d12ed8bc00caac00d228b5c50a
brendaffunchio <49459078+brendaffunchio@users.noreply.github.com>
2022-07-07 12:59:37 -0300

    OT229-66: Added method to update testimonials in service

--- src/main/java/com/alkemy/ong/services/TestimonialService.java
+++ src/main/java/com/alkemy/ong/services/TestimonialService.java
@@ -4,8 +4,10 @@ import com.alkemy.ong.dto.TestimonialDTORequest;
 import com.alkemy.ong.dto.TestimonialDTOResponse;
 import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.exception.CorruptedFileException;
+import javassist.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface TestimonialService {
     TestimonialDTOResponse create(MultipartFile file, TestimonialDTORequest request)throws CloudStorageClientException, CorruptedFileException;
+    TestimonialDTOResponse update(String id,MultipartFile file, TestimonialDTORequest request) throws CloudStorageClientException, CorruptedFileException, NotFoundException;
 }
--- src/main/java/com/alkemy/ong/services/impl/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/TestimonialServiceImpl.java
@@ -9,6 +9,7 @@ import com.alkemy.ong.mappers.TestimonialMapper;
 import com.alkemy.ong.repositories.TestimonialRepository;
 import com.alkemy.ong.services.CloudStorageService;
 import com.alkemy.ong.services.TestimonialService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,5 +36,20 @@ public class TestimonialServiceImpl implements TestimonialService {
         repository.save(testimonial);
         return mapper.testimonialEntity2DTOResponse(testimonial);
 
+    }
+
+    @Override
+    public TestimonialDTOResponse update(String id, MultipartFile file, TestimonialDTORequest request) throws CloudStorageClientException, CorruptedFileException, NotFoundException {
+        Boolean exists = repository.existsById(id);
+        if (!exists)throw new NotFoundException("Error: Testimonial with id " + id + " was not found");
+        Testimonial testimonialFound= repository.getById(id);
+        if(file != null && !file.isEmpty()){
+            testimonialFound.setImage(amazonS3Service.uploadFile(file));
+        }
+        if(!request.getName().isEmpty()) testimonialFound.setName(request.getName());
+        if(!request.getContent().isEmpty()) testimonialFound.setContent(request.getContent());
+
+        repository.save(testimonialFound);
+        return mapper.testimonialEntity2DTOResponse(testimonialFound);
     }
 }
