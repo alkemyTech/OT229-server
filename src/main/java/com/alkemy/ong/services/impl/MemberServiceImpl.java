a46f943c34700a637375c01f54f504857b3f04ec
Mariano Vergara <marianovf19@outlook.com>
2022-07-08 10:59:01 -0300

    Se implemento el metodo delete de Member y tambien se implemento un metodo privado para eliminar la imagen del servidor de amazon

--- src/main/java/com/alkemy/ong/services/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/MemberServiceImpl.java
@@ -1,13 +1,21 @@
 package com.alkemy.ong.services.impl;
 
+import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.dto.MemberDTORequest;
 import com.alkemy.ong.dto.MemberDTOResponse;
 import com.alkemy.ong.entities.Member;
+import com.alkemy.ong.exception.CloudStorageClientException;
+import com.alkemy.ong.exception.CorruptedFileException;
+import com.alkemy.ong.exception.EntityImageProcessingException;
+import com.alkemy.ong.exception.FileNotFoundOnCloudException;
 import com.alkemy.ong.mappers.MemberMapper;
 import com.alkemy.ong.repositories.MembersRepository;
+import com.alkemy.ong.services.CloudStorageService;
 import com.alkemy.ong.services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 
 import java.util.regex.Pattern;
 
@@ -36,5 +44,32 @@ public class MemberServiceImpl implements MemberService {
         }
 
 
+    }
+    @Override
+    @Transactional
+    public MemberDTO deleteMember(String id) throws EntityNotFoundException,CloudStorageClientException {
+        Member memberToDelete = membersRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Member with the provided id not found."));
+        try{
+            this.deleteMemberImageFromCloudStorage(memberToDelete);
+        } catch (FileNotFoundOnCloudException e){
+
+        }
+        MemberDTO memberDTO = this.memberMapper.memberEntity2DTOMember(memberToDelete);
+        this.membersRepository.delete(memberToDelete);
+        return memberDTO;
+    }
+
+    private void deleteMemberImageFromCloudStorage(Member member)throws CloudStorageClientException, FileNotFoundOnCloudException{
+        String urlImage = member.getImage();
+        if (urlImage != null && urlImage.equals("")){
+            try {
+                this.cloudStorageService.deleteFileFromS3Bucket(urlImage);
+            } catch (EntityImageProcessingException e){
+                if (!(e instanceof FileNotFoundOnCloudException)){
+                    throw e;
+                }
+            }
+        }
     }
 }
