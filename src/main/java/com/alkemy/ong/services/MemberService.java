8ebd3618451c9ea171b762fe2aedc558f835baf1
Mariano Vergara <marianovf19@outlook.com>
2022-07-08 10:54:29 -0300

    Se agrego el metodo delete en la interfaz de Miembro y se modifico el metodo para aceptar imagenes al crear un miembro

--- src/main/java/com/alkemy/ong/services/MemberService.java
+++ src/main/java/com/alkemy/ong/services/MemberService.java
@@ -1,12 +1,17 @@
 package com.alkemy.ong.services;
 
+import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.dto.MemberDTORequest;
 import com.alkemy.ong.dto.MemberDTOResponse;
 import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.exception.CorruptedFileException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+
 public interface MemberService {
-    MemberDTOResponse create(MemberDTORequest request) throws Exception;
+    MemberDTOResponse create(MemberDTORequest request) throws CloudStorageClientException, CorruptedFileException;
+    MemberDTO deleteMember(String id) throws EntityNotFoundException,CloudStorageClientException;
 }
 
