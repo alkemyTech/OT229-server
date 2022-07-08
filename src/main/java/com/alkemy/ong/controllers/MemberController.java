ed1734ad088edd9c70084b17f2a96862286cac20
Mariano Vergara <marianovf19@outlook.com>
2022-07-08 10:52:53 -0300

    Se agrego el endpoint para eliminar Miembro

--- src/main/java/com/alkemy/ong/controllers/MemberController.java
+++ src/main/java/com/alkemy/ong/controllers/MemberController.java
@@ -1,16 +1,22 @@
 package com.alkemy.ong.controllers;
 
+import com.alkemy.ong.dto.DeleteEntityResponse;
+import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.dto.MemberDTORequest;
+import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.services.MemberService;
 import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 @RestController
@@ -25,6 +31,15 @@ public class MemberController {
             return new ResponseEntity<>(memberService.create(request), HttpStatus.CREATED);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @DeleteMapping
+    public ResponseEntity<?> deleteMember(@PathVariable String id)throws CloudStorageClientException {
+        try {
+            MemberDTO memberDTO = memberService.deleteMember(id);
+            return ResponseEntity.ok(new DeleteEntityResponse("Member successfully deleted.", memberDTO));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
