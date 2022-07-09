7582484b20b7ec1168e17d1099e1cdfc0aa2526a
Franco Roman <franco.3271940@gmail.com>
2022-07-08 23:44:02 -0300

    Method to update a comment added and implemented

--- src/main/java/com/alkemy/ong/services/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.services.impl;
 import com.alkemy.ong.dto.CommentDTO;
 import com.alkemy.ong.entities.CommentEntity;
 import com.alkemy.ong.entities.User;
+import com.alkemy.ong.mappers.CommenttMapper;
 import com.alkemy.ong.repositories.CommentRepository;
 import com.alkemy.ong.security.service.JwtService;
 import com.alkemy.ong.services.CommentService;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +19,9 @@ public class CommentServiceImpl implements CommentService {
 
     @Autowired
     CommentRepository commentRepository;
+
+    @Autowired
+    CommenttMapper commenttMapper;
 
     @Autowired
     JwtService jwtService;
@@ -32,6 +35,29 @@ public class CommentServiceImpl implements CommentService {
     }
 
     @Override
+    public CommentDTO updateComment(String idComentary, String newCommentBody, String token) throws Exception {
+        Optional<CommentEntity> commentFound = commentRepository.findById(idComentary);
+
+        if(commentFound.isPresent()){
+            List<String> roles = jwtService.getRoles(token);
+
+            String userName = jwtService.getUsername(token); // El username del token es el correo
+            User user = userService.getUserByEmail(userName).get();
+
+            if(checkPermissions(roles, commentFound.get().getId(), user.getId())){
+                commentFound.get().setBody(newCommentBody);
+
+                commentRepository.save(commentFound.get());
+                return commenttMapper.entity2DTO(commentFound.get());
+            }else{
+                throw new Exception("You don't have permissions to edit this comment");
+            }
+        }else{
+            throw new EntityNotFoundException("Comment with the provided ID not present");
+        }
+    }
+
+    @Override
     public String deleteComment(String idComentary, String token) throws Exception {
         Optional<CommentEntity> commentFound = commentRepository.findById(idComentary);
 
@@ -41,20 +67,18 @@ public class CommentServiceImpl implements CommentService {
             String userName = jwtService.getUsername(token); // El username del token es el correo
             User user = userService.getUserByEmail(userName).get();
 
-            // Si es administrador lo puede eliminar
-            if(roles.contains("ROLE_ADMIN")){
+            if(checkPermissions(roles, commentFound.get().getId(), user.getId())){
                 commentRepository.deleteById(commentFound.get().getId());
                 return "Successfully deleted comment";
-
-            }else if(commentFound.get().getUserId().equals(user.getId())){ // Verifico si es el propietario del comentario
-                commentRepository.deleteById(commentFound.get().getId());
-                return "Successfully deleted comment";
-
             }else{
                 throw new Exception("You don't have permissions to delete this comment");
             }
         }else{
             throw new EntityNotFoundException("Comment with the provided ID not present");
         }
+    }
+
+    private boolean checkPermissions(List<String> roles, String idComment, String idUser){
+        return roles.contains("ROLE_ADMIN") || idComment.equals(idUser) ? true : false;
     }
 }
