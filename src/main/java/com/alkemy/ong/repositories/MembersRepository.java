6599afbd6b728a41ef6838e72930b055f62748c3
Ivan Pizarro <pizarroivan89@gmail.com>
2022-07-10 20:28:57 -0300

    Search method added to the repository.

--- src/main/java/com/alkemy/ong/repositories/MembersRepository.java
+++ src/main/java/com/alkemy/ong/repositories/MembersRepository.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.entities.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MembersRepository extends JpaRepository<Member, String> {
+
+    Page<Member> findAll(Pageable pageRequest);
 
 }
