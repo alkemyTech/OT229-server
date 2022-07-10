11a584670aa75d08241bb6d27ad86c704cf251f2
Ivan Pizarro <pizarroivan89@gmail.com>
2022-07-10 20:32:40 -0300

    Method with the main functionality declared in the service interface.

--- src/main/java/com/alkemy/ong/services/MemberService.java
+++ src/main/java/com/alkemy/ong/services/MemberService.java
@@ -2,9 +2,11 @@ package com.alkemy.ong.services;
 
 import com.alkemy.ong.dto.MemberDTORequest;
 import com.alkemy.ong.dto.MemberDTOResponse;
+import com.alkemy.ong.dto.PageResultResponse;
 import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.exception.CorruptedFileException;
 import com.alkemy.ong.exception.FileNotFoundOnCloudException;
+import com.alkemy.ong.exception.PageIndexOutOfBoundsException;
 import javassist.NotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityNotFoundException;
@@ -14,5 +16,19 @@ public interface MemberService {
     MemberDTOResponse create(MultipartFile file,MemberDTORequest request) throws CloudStorageClientException, CorruptedFileException;
     String deleteMember(String id) throws NotFoundException, CloudStorageClientException, FileNotFoundOnCloudException;
     List<MemberDTOResponse> getAllMembers();
+
+    /**
+     * Performs a paginated search for all the Member entries in the database and returns them along with the urls
+     * to get the previous and next page results.
+     *
+     * <p>Page size and sorting criteria are read from global constants.
+     *
+     * @param pageNumber    the index of the page to be retrieved.
+     * @return  the list of Members. If the provided index exceeds the last existing index, an empty list will
+     *          be returned, and the previous page url attribute will point to the last available page.
+     * @throws PageIndexOutOfBoundsException    if the index is not a positive integer.
+     */
+    PageResultResponse<MemberDTOResponse> getAllMembers(int pageNumber) throws PageIndexOutOfBoundsException;
+
 }
 
