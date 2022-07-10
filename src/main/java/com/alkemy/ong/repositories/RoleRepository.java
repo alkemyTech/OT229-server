29d0e7c1e479fad067b43b19de9f7dd540f5830b
exerazor1911 <72807447+exerazor1911@users.noreply.github.com>
2022-07-10 17:45:44 -0300

    ot229-72-refactor (#89)
    * ot229-72-refactor
    
    * Refactored / cleansed
    
    * refactored again
    
    Co-authored-by: exerazor1911 <ezequielgiussani56@gmail.com>

--- src/main/java/com/alkemy/ong/repositories/RoleRepository.java
+++ src/main/java/com/alkemy/ong/repositories/RoleRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role,String> {
     Optional<Role> findByName(String roleName);
+
+    Boolean existsByName(String name);
 }
