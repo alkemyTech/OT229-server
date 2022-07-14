d7b82fd55f29b910ccfb7bbfd7dd1863ae83959e
Ivan Pizarro <pizarroivan89@gmail.com>
2022-07-14 14:09:54 -0300

    OpenApi config with JWT created

---
+++ src/main/java/com/alkemy/ong/configuration/OpenApiConfiguration.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "OT229", version = "0.1.0"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfiguration {
+}
