bf8f41e2d7c26738084d1b8783e8ce5fc7232882
Camilo Abarca <85465518+cabarcaz@users.noreply.github.com>
2022-07-15 12:56:23 -0400

    Agregar documentación a Testimonios - Ot229 88 (#101)
    * Se documenta Testimonial
    
    * Se agrega documentacion a dto
    
    * se agrega jwt

--- src/main/java/com/alkemy/ong/controllers/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controllers/TestimonialController.java
@@ -1,11 +1,19 @@
 package com.alkemy.ong.controllers;
 
 import com.alkemy.ong.dto.TestimonialDTORequest;
+import com.alkemy.ong.dto.TestimonialDTOResponse;
 import com.alkemy.ong.exception.CloudStorageClientException;
 import com.alkemy.ong.exception.CorruptedFileException;
 import com.alkemy.ong.exception.FileNotFoundOnCloudException;
 import com.alkemy.ong.services.TestimonialService;
 import com.alkemy.ong.utility.GlobalConstants;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,11 +30,34 @@ public class TestimonialController {
     @Autowired
     private TestimonialService service;
 
+    @Operation(summary = "Create a new testimonial", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201",description = "Testimonial successfully created",
+        content = {
+            @Content(mediaType = "application/json", schema=@Schema(implementation =  TestimonialDTOResponse.class))
+        }),
+            @ApiResponse(responseCode="404", description = "Testimonial could not be created",
+                content = {
+                @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))
+            })
+    })
     @PostMapping
     public ResponseEntity<?> createTestimonial(@Valid @ModelAttribute TestimonialDTORequest request,
                                                @RequestParam("file") MultipartFile file) throws CloudStorageClientException, CorruptedFileException {
         return new ResponseEntity<>(service.create(file,request), HttpStatus.CREATED);
     }
+
+    @Operation(summary = "Update an existing testimonial", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Testimonial successfully updated",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = TestimonialDTOResponse.class))
+            }),
+        @ApiResponse(responseCode = "404", description = "Testimonial not found",
+            content = {
+            @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))
+        }),
+    })
     @PutMapping
     public ResponseEntity<?> updateTestimonial(@Valid @ModelAttribute TestimonialDTORequest request,
                                                @RequestParam(value = "file", required = false) MultipartFile file,
@@ -38,6 +69,18 @@ public class TestimonialController {
        }
 
     }
+
+    @Operation(summary = "Delete a testimonial", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Testimonial successfully deleted",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = TestimonialDTOResponse.class))
+            }),
+        @ApiResponse(responseCode = "404", description = "Testimonial not found",
+            content = {
+            @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))
+        }),
+    })
     @DeleteMapping
     public ResponseEntity<?>deleteTestimonial(@RequestParam("id")String id)throws CloudStorageClientException, FileNotFoundOnCloudException {
 
