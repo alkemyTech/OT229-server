d91c1f180e63504231248f6734ef521c47c163f6
Ivan Pizarro <pizarroivan89@gmail.com>
2022-07-15 14:27:43 -0300

    Se adapta el posterior PR OT229-88 con la doc de Testimonial

--- src/main/java/com/alkemy/ong/dto/EncodedImageDTO.java
+++ src/main/java/com/alkemy/ong/dto/EncodedImageDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,10 @@ import javax.validation.constraints.NotBlank;
 public class EncodedImageDTO {
 
     @NotBlank
+    @Schema(description = "String representing the encoded image in base64. Will be quite long.", example = "/9j/4AAQSkZJRgABAQEAYABgA")
     private String encoded_string;
     @NotBlank
+    @Schema(description = "The file name with its extension.", example = "file_name.jpg")
     private String file_name;
 
 }
