cd3405adb718900175a654225ea7e23aa36e7c71
Franco Roman <franco.3271940@gmail.com>
2022-07-19 00:53:08 -0300

    New documentation added

--- src/main/java/com/alkemy/ong/dto/NewsDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewsDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,13 +11,19 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class NewsDTO {
   private String id;
+
   @NotEmpty(message = "name must be provided")
+  @Schema(description = "The News's name.", example = "Weather today")
   private String name;
+
   @NotEmpty(message = "content must be provided")
+  @Schema(description = "The News's content.", example = "Today will be sunny")
   private String content;
 
   private String image;
+
   @NotNull(message = "category must be provided")
+  @Schema(description = "Category to which the news belongs", example = "Weather")
   private CategoryDTO category;
 
 }
--- src/main/java/com/alkemy/ong/dto/NewsDTORequest.java
+++ src/main/java/com/alkemy/ong/dto/NewsDTORequest.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewsDTORequest extends NewsDTO {
-
+    @Valid
     private EncodedImageDTO encoded_image;
 
 }
