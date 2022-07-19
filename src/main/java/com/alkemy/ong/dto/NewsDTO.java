52daf9c9e3abad4384e3f663efcaa9e5d3f9f143
Franco Roman <franco.3271940@gmail.com>
2022-07-19 01:01:05 -0300

    Documentation added to image atribute

--- src/main/java/com/alkemy/ong/dto/NewsDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewsDTO.java
@@ -20,6 +20,7 @@ public class NewsDTO {
   @Schema(description = "The News's content.", example = "Today will be sunny")
   private String content;
 
+  @Schema(description = "Url of imaged was save in the cloud", example = "https://cohorte-junio-a192d78b.s3.amazonaws.com/1657487943605-newsImage.png")
   private String image;
 
   @NotNull(message = "category must be provided")
