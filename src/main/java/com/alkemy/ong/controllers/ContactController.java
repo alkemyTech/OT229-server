e8a1625222d348d9d09b3992fcfd035bcac2ee2f
Brenda Daffunchio <brendaffunchio@gmail.com>
2022-07-19 00:23:55 -0300

    OT229-95 Test de endpoint de contacto (#110)
    * finished tests
    
    * another test added
    
    * finished all tests. 100% coverage

--- src/main/java/com/alkemy/ong/controllers/ContactController.java
+++ src/main/java/com/alkemy/ong/controllers/ContactController.java
@@ -7,9 +7,7 @@ import com.alkemy.ong.utility.GlobalConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +23,7 @@ public class ContactController {
     private ContactService service;
 
     @PostMapping
-    public ResponseEntity<?> createContact(@Valid @ModelAttribute ContactDTORequest request) {
+    public ResponseEntity<?> createContact(@Valid @RequestBody ContactDTORequest request) {
         try {
             return new ResponseEntity<>(service.create(request), HttpStatus.CREATED);
         } catch (Exception e) {
