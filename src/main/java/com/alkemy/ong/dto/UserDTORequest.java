7bdf4e1821a40237010b4fa708f4f5cf43313832
Franco Roman <franco.3271940@gmail.com>
2022-07-19 19:03:11 -0300

    All test done - 100% coverage | Anotations added in request DTO

--- src/main/java/com/alkemy/ong/dto/UserDTORequest.java
+++ src/main/java/com/alkemy/ong/dto/UserDTORequest.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @NoArgsConstructor
@@ -14,13 +16,24 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class UserDTORequest {
+    @NotBlank
     private String id;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank
     @Size(min = 1, max = 255)
     private String password;
+
+    @Valid
     private EncodedImageDTO encoded_image;
 
 
