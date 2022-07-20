ab3d4d619d82dba6e15df665489e371196b2960d
Franco <50601878+Franco940@users.noreply.github.com>
2022-07-20 00:25:38 -0300

    OT229-86 Agregar documentación a Novedades (#112)
    * Added documentation in all endpoints
    
    * New documentation added
    
    * Documentation added to image atribute
    
    * Little refactor

--- src/main/java/com/alkemy/ong/controllers/NewsController.java
+++ src/main/java/com/alkemy/ong/controllers/NewsController.java
@@ -9,6 +9,11 @@ import com.alkemy.ong.exception.PageIndexOutOfBoundsException;
 import com.alkemy.ong.services.CloudStorageService;
 import com.alkemy.ong.services.NewsService;
 import com.alkemy.ong.utility.GlobalConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
-import java.io.IOException;
 
 @RestController
 @RequestMapping(GlobalConstants.Endpoints.NEWS)
@@ -28,11 +32,28 @@ public class NewsController {
   private CloudStorageService cloudStorageService;
 
 
+  @Operation(summary = "Create and save a new News entity")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Return the News entity created successfully", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDTO.class))})
+          }
+  )
   @PostMapping
   public ResponseEntity<NewsDTO> save(@Valid @RequestBody NewsDTORequest newsDTO) throws Exception{
     return ResponseEntity.status(HttpStatus.CREATED).body(this.newsService.save(newsDTO));
   }
 
+  @Operation(summary = "Search a news with an ID")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Returns the News found in the database with the provided ID", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDTO.class))}),
+
+                  @ApiResponse(responseCode = "404", description = "News not found in the database with the provided ID", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})
+          }
+  )
   @GetMapping("/{id}")
   public ResponseEntity<?> findById(@PathVariable String id){
     NewsDTO newsDTO=null;
@@ -45,6 +66,16 @@ public class NewsController {
       return ResponseEntity.status(HttpStatus.OK).body(newsDTO);
   }
 
+  @Operation(summary = "Delete a New with an ID")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Return the News that was deleted", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDTO.class))}),
+
+                  @ApiResponse(responseCode = "404", description = "News not found in the database with the provided ID", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})
+          }
+  )
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deleteNews(@PathVariable String id) throws CloudStorageClientException {
     try {
@@ -55,6 +86,19 @@ public class NewsController {
     }
   }
 
+  @Operation(summary = "Udpate a News with an ID")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Return the News that was updated", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDTO.class))}),
+
+                  @ApiResponse(responseCode = "404", description = "News not found in the database with the provided ID", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))}),
+
+                  @ApiResponse(responseCode = "400", description = "Invalid model attribute", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})
+          }
+  )
   @PutMapping("/{id}")
   public ResponseEntity<?> updateNews(@Valid @RequestBody NewsDTORequest updatedNews,
                                       @PathVariable String id) throws CloudStorageClientException, CorruptedFileException {
@@ -68,6 +112,13 @@ public class NewsController {
     }
   }
 
+  @Operation(summary = "Get all news")
+  @ApiResponses(
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Returns all news founded", content = {
+                          @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDTO.class))})
+          }
+  )
   @GetMapping
   public ResponseEntity<?> getAllNews(@RequestParam(value = GlobalConstants.PAGE_INDEX_PARAM) int page) throws PageIndexOutOfBoundsException {
     return ResponseEntity.ok( this.newsService.getAllNews(page) );
--- src/main/java/com/alkemy/ong/dto/NewsDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewsDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,13 +11,20 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class NewsDTO {
   private String id;
+
   @NotEmpty(message = "name must be provided")
+  @Schema(description = "The News's name.", example = "Weather today")
   private String name;
+
   @NotEmpty(message = "content must be provided")
+  @Schema(description = "The News's content.", example = "Today will be sunny")
   private String content;
 
+  @Schema(description = "Url of image was save in the cloud", example = "https://cohorte-junio-a192d78b.s3.amazonaws.com/1657487943605-newsImage.png")
   private String image;
+
   @NotNull(message = "category must be provided")
+  @Schema(description = "Category to which the news belongs", example = "Weather")
   private CategoryDTO category;
 
 }
